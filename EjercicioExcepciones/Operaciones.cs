5d9df49d4cbbb588788e4e040e6f23a0f8316394
osemartinez <140188644+osemartinez@users.noreply.github.com>
2023-11-13 14:49:45 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioExcepciones/Operaciones.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioExcepciones
+{
+    public class Operaciones
+    {
+        //Creamos los metodos para las operaciones:
+
+
+
+        //Metodo para sumar los dos números
+        public double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+
+
+        //Metodo para restar los dos números
+        public double Restar(double a, double b)
+        {
+            return a - b;
+        }
+
+
+        //Metodo para multplicar los dos números
+        public double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+
+        //Metodo para dividir los dos números, aca podemos lanzar una excepcion por si se quiere dividir por cero
+        public double Dividir(double a, double b)
+        {
+            if (b == 0)
+            {
+                // Excepcion si se quiere dividir por cero 
+                throw new DivideByZeroException();   
+            }
+
+            return a / b;
+        }
+    }
+}
---
+++ EjercicioExcepciones/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EjercicioExcepciones;
+
+Console.WriteLine("Hello, World!");
+
+try
+{
+    //solicitar primer numero
+    Console.WriteLine("Ingrase el primer número: ");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+
+    //Solicitar segundo numero
+    Console.WriteLine("Ingrese el segundo número");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+
+    
+    
+    
+    
+    Operaciones operaciones = new Operaciones();
+
+    // Realizar operaciones y mostrar resultados
+    Console.WriteLine("Suma: " + operaciones.Sumar(num1, num2));
+    Console.WriteLine("Resta: " + operaciones.Restar(num1, num2));
+    Console.WriteLine("Multiplicación: " + operaciones.Multiplicar(num1, num2));
+
+    try
+    {
+        Console.WriteLine("División: " + operaciones.Dividir(num1, num2));
+    }
+
+    // Aca manejamos la excepción de la división por cero
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("No se puede dividir por cero.");
+    }
+}
+    
+
+
+    // Aca podeos manejar la excepción si el usuario no ingresa un número
+    catch (FormatException)
+    {
+          Console.WriteLine("Error: Ingrese números válidos.");
+    }
+   
+
+
+     
+           
